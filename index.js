98cf297ea0399f400bf7bf811e9f45f535d25f0f
Alexandra Fren <alexandra.fren@gmail.com>
2018-12-15 19:11:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function produceDrivingRange(blockRange) {
   return function rangeCalculator(startingBlock, endingBlock){
-
+    let distance = Math.abs((parseInt(endingBlock)) - (parseInt(startingBlock)));
+    let rangeValue = blockRange - distance;
+    if (rangeValue > 0) {
+      return "within range by " + rangeValue;
+    }
+    else {
+      rangeValue = Math.abs(rangeValue);
+      return rangeValue + " blocks out of range";
+    }
   };
 }
 
